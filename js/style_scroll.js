8653f81093f7daf8d587f32c5d9ff818c1faf875
unknown <C:\Program Files\The Bat!\Mail>
2018-06-10 22:55:47 +0300

    Final

---
+++ js/style_scroll.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".acc-body").niceScroll({
+    cursorcolor: "#95e1d3",
+    cursoropacitymin: "0.5",
+    cursorwidth: "8px"
+  });
+  // $(".slaider").easySlider({
+  //   auto: true,
+  //   continuous: true,
+  //   nextId: "slider1next",
+  //   prevId: "slider1prev"
+  // });
+  $(".slaider").slick();
+});
